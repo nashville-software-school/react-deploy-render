31ab49686a81edeab71642610cc1056f3e13f3fb
Andy Collins <askingalot@gmail.com>
2019-02-28 17:47:26 -0600

    fix for app refresh

--- server.js
+++ server.js
@@ -4,9 +4,21 @@ const server = jsonServer.create();
 const router = jsonServer.router('./API/db.json');
 const middlewares = jsonServer.defaults({ static: "./build" });
 const port = process.env.PORT || 3000;
+const dbRoutes = Object.keys(require('./API/db.json'));
 
 server.use(middlewares);
+
+server.use((req, res, next) => {
+    if (req.path === '/' || dbRoutes.some(r => req.path.endsWith(r))) {
+        next();
+    } else {
+        res.redirect('/');
+    }
+})
+
+
 server.use(router);
+
 server.listen(port, () => {
     console.log(`app running on port ${port}`);
 });
